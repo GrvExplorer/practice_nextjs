63cd4305e83abd1426c13ac65279479aa2e47c91
GrvExplorer <icodelife307@gmail.com>
2024-06-21 01:42:33 +0530

    error at middleware

--- blog-app/src/middleware.ts
+++ blog-app/src/middleware.ts
@@ -1,18 +1,26 @@
-import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
- 
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 
-  // const isAuthenticated = false
-  // if (isAuthenticated) {
-  //   return NextResponse.redirect(new URL('/blogs', request.url))
-  // }
-  // return NextResponse.redirect(new URL('auth/sign-in', request.url))
+  const path = request.nextUrl.pathname
+  const getCookies = cookies()
+  const token = getCookies.get('token')?.value || ''
+
+  const publicPath = path === '/auth/sign-in' || '/auth/sign-up'
+
+  if (publicPath && token !== '') {
+    return NextResponse.redirect(new URL('/blogs', request.nextUrl))
+  }
+
+  if (!publicPath && token === '') {
+    return NextResponse.redirect(new URL('/auth/sign-in', request.nextUrl))
+  }
 
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/', '/blogs'],
-}
+  matcher: ['/auth/sign-in', '/auth/sign-up'],
+};
